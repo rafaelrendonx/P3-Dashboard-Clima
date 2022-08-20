672d92d83d743b8722429f926532adbe2d4a414d
rafaelrendonx <rafael_x@hotmail.com>
2022-08-19 17:09:23 -0700

    Se agrega primer GET(coordenadas)

--- apis.js
+++ apis.js
@@ -10,43 +10,41 @@ let ciudades = [];
 enviarCiudad.addEventListener("submit", function (event) {
     event.preventDefault();
     guardarCiudad(recibirCiudad.value)
-    console.log(ciudades)
     mostrarCiudad();
+    peticion();
 });
+
 
 //Funcion para guardar varias ciudades
 function guardarCiudad (ciudad) {
     ciudades.push({
         ciudad
     })
+    console.log(ciudades)
 }
 
 //Funcion para mostrar ciudad
 function mostrarCiudad () {
     imprimirCiudad.innerHTML = ""
-    ciudades.forEach(function () {
+    ciudades.forEach(function (objeto) {
     imprimirCiudad.innerHTML += `
     <div>
-        <h3>${recibirCiudad.value}</h3>
+        <h3>${objeto.ciudad}</h3>
         <p id="temperatura">Temperatura</p>
         <p id="humedad">Humedad</p>
         <p id="viento">Viento</p>
     </div>
     `
     })
-    
 }
 
-
 //Peticion para buscar coordenadas de ciudad
-let URL = "https://api.openweathermap.org/geo/1.0/direct?q=Hermosillo&limit=5&appid=63a2a74129b1c74b998c9d25633632ad";
-
 const peticion = async () => {
-    const respuesta = await axios.get("https://api.openweathermap.org/geo/1.0/direct?q=Hermosillo&limit=5&appid=63a2a74129b1c74b998c9d25633632ad", {
+    const respuesta = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${recibirCiudad.value}&limit=5&appid=63a2a74129b1c74b998c9d25633632ad`, {
     })
     console.log(respuesta.data[0].lat)
     console.log(respuesta.data[0].lon)
 }
 
-//peticion();
+
 
